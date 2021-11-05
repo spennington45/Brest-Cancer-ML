f65fc3c156469ed657f2bc4928976248fbc6ead9
smpennington <spennington@herzog.com>
2021-11-05 15:43:01 -0500

    finished with data set still need a plot of some kind

--- breastCancerML.py
+++ breastCancerML.py
@@ -1,9 +1,22 @@
-import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix, accuracy_score
-from matplotlib.colors import ListedColormap
 
+data = pd.read_csv("./Logistic_Regression/Final Folder/Dataset/breast_cancer.csv")
+X = data.iloc[:, 1:-1].values
+y = data.iloc[:, -1].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=0)
+
+classifier = LogisticRegression(random_state=0)
+classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
+
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+
+accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10)
+print("Accuracy: {:.2f}%".format(accuracies.mean()*100))
+print("Standard Deviation: {:.2f}%".format(accuracies.std()*100))

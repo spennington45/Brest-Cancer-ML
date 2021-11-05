6c24c61251c849bca437709d37a5a4732cf09ede
smpennington <spennington@herzog.com>
2021-11-05 12:13:06 -0500

    init commit with file

---
+++ breastCancerML.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix, accuracy_score
+from matplotlib.colors import ListedColormap
+
